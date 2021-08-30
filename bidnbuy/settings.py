f3c29208f634d08042458e29da00d8d16381aa0c
Darius Feher <darius.feher@student.manchester.ac.uk>
2021-08-30 19:23:13 +0300

    Change .env location

--- bidnbuy/settings.py
+++ bidnbuy/settings.py
@@ -28,7 +28,9 @@ SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+# print(SECRET_KEY)
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
