f3997c68b1833cdb4e09fe84c6ac7358fa458b16
Darius Feher <darius.feher@student.manchester.ac.uk>
2021-09-07 03:14:43 +0300

    Add import to settings

--- bidnbuy/settings.py
+++ bidnbuy/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import sys
 from pathlib import Path
 
 import dj_database_url
@@ -28,7 +29,7 @@ SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 # print(SECRET_KEY)
 
 
